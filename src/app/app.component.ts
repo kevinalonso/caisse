84a2acdb422395d3f6bc713b70a052e84cddbbea
Kevin <k.alonso@groupe-isore.fr>
2020-02-28 13:38:44 +0100

    Start to create admin component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'votre caisse enregistreuse';
+  login;
+
+  constructor(private router : Router){}
 
   connexion(){
-    alert("lol");
-    /*if (this.login == "kalons") {
-
-    }*/
+    if (this.login == "kalons") {
+      //permet de cacher la partie haute de la page de connecion
+      document.getElementById('login-container').style.visibility = 'hidden';
+      document.getElementById('login-container').style.height = '0px';
+      this.router.navigate(['/home']);
+    }
   }
 
 }
